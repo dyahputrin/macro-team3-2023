e2c2ab5b2b79a01a713b195be00fcaca0a3965ae
dyahputrin <putri.nariswari@gmail.com>
2023-10-09 13:41:35 +0700

    initial commit

---
+++ macro2023/macro2023App.swift
@@ -0,0 +1,17 @@
+//
+//  macro2023App.swift
+//  macro2023
+//
+//  Created by Dyah Putri Nariswari on 09/10/23.
+//
+
+import SwiftUI
+
+@main
+struct macro2023App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
