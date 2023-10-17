37d5003676fd994b667bae710932d39f00010ca9
dyahputrin <putri.nariswari@gmail.com>
2023-10-17 14:57:46 +0700

    commit

--- macro2023/view/CanvasView.swift
+++ macro2023/view/CanvasView.swift
@@ -1,171 +1,67 @@
-//
-//  CanvasView.swift
-//  macro2023
-//
-//  Created by Shalomeira Winata on 11/10/23.
-//
-
 import SwiftUI
+import SceneKit
 
 struct CanvasView: View {
-    @State private var sheetPresented = true
-    @State private var isSetButtonTapped = false
-    @State var objectsButtonClicked: Bool
-    @State var roomButtonClicked: Bool
+    @Environment(\.managedObjectContext) private var viewContext
     
-    @Binding var viewfinderButtonClicked: Bool
-    @Binding var isImporting: Bool
-    @Binding var isExporting: Bool
-    @Binding var isSetButtonSidebarTapped: Bool
-    
-    @StateObject private var roomSceneViewModel = RoomSceneViewModel(roomSceneModel: RoomSceneModel(roomWidth: 0, roomHeight: 0, roomLength: 0))
-    @State private var sceneViewID = UUID()
-    @State var roomWidthText: String = ""
-    @State var roomLengthText: String = ""
-    @State var roomHeightText: String = ""
-    
-    @State private var showSaveAlert = false
-    @State private var projectName = "New Project"
-    @State private var newProjectName = ""
-    @State private var renameClicked = false
-    
-    @State private var isRoomCaptureViewPresented = false
+    @EnvironmentObject var routerView:RouterView
+    @StateObject var dataCanvasViewModel = CanvasViewModel()
     
     var body: some View {
-        
-        GeometryReader { geometry in
-            ZStack {
-                RoomSceneView(roomSceneViewModel: roomSceneViewModel, sceneViewID: $sceneViewID)
-            }
+        HStack {
+            SceneView(
+                scene: dataCanvasViewModel.sceneSpawn(),
+                pointOfView: dataCanvasViewModel.cameraNode(),
+                options: [
+                    .allowsCameraControl,
+                    .autoenablesDefaultLighting,
+                    .temporalAntialiasingEnabled
+                ]
+            )
+            .frame(width: UIScreen.main.bounds.width / 4 * 3, height: UIScreen.main.bounds.height / 8 * 7)
+            .border(Color.black)
             
-            if objectsButtonClicked == true {
-                ObjectSidebarView()
-                    .transition(.moveAndFade)
-                
-            } else if roomButtonClicked == true {
-                RoomSidebarView(roomWidthText: $roomWidthText, roomLengthText: $roomLengthText, roomHeightText: $roomHeightText,  sceneViewID: $sceneViewID, roomSceneViewModel: roomSceneViewModel)
-                    .transition(.moveAndFade)
-            }
+            Text("Hello, World!")
+                .frame(width: UIScreen.main.bounds.width / 4, height: UIScreen.main.bounds.height / 8 * 7)
+                .border(Color.black)
         }
-        .onAppear {
-            sheetPresented = true
-        }
-        .sheet(isPresented: $sheetPresented) {
-            SizePopUpView(sheetPresented: $sheetPresented, isSetButtonTapped: $isSetButtonTapped, roomSceneViewModel: roomSceneViewModel, sceneViewID: $sceneViewID, roomWidthText: $roomWidthText, roomLengthText: $roomLengthText, roomHeightText: $roomHeightText)
-                .interactiveDismissDisabled()
-        }
-        .toolbarRole(.editor)
-        .toolbarBackground(Color.blue)
+        .padding()
         .toolbar {
-            ToolbarItemGroup {
-                HStack {
-                    //VIEWFINDER
-                    Menu {
-                        Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
-                            Text("Scan objects")
-                        })
-                        Button(action: {
-                            self.isRoomCaptureViewPresented = true
-                        }, label: {
-                            Text("Scan room")
-                        })
-                    } label: {
-                        Label("Viewfinder", systemImage: "viewfinder")
-                            .foregroundColor(.black)
-                    }
-                    
-                    // ROOM
-                    Button(action : {
-                        roomButtonClicked.toggle()
-                        objectsButtonClicked = false
-                    }) {
-                        Image(systemName: "square.split.bottomrightquarter")
-                            .foregroundColor(roomButtonClicked ? .blue : .black)
-                            .padding()
-                    }
-                    
-                    //OBJECTS
-                    Button(action : {
-                        objectsButtonClicked.toggle()
-                        roomButtonClicked = false
-                    }) {
-                        Image(systemName: "chair.lounge")
-                            .foregroundColor(objectsButtonClicked ? .blue : .black)
-                    }
+            ToolbarItemGroup(placement: .topBarLeading) {
+                Button() {
+                    routerView.path.removeLast()
+                } label: {
+                    Image(systemName: "chevron.left")
                 }
-                .padding(.trailing, 100)
-            }
-            
-            // UNDO & SAVE
-            ToolbarItemGroup {
-                Button(action: {})
-                {
-                    Image(systemName: "arrow.uturn.backward.circle")
+                NavigationLink(destination:ContentView()
+                               ,label: {
+                    //   Text(routerView.project == nil ? dataCanvasViewModel.dataCanvas.nameProject : routerView.project)
+                    Text(routerView.project?.projectName ?? dataCanvasViewModel.dataCanvas.nameProject )
                         .foregroundColor(.black)
-                        .padding()
+                    Image(systemName: "pencil")
                 }
-                Button(action: {
-                    showSaveAlert = true
-                })
-                {
-                    Image(systemName: "square.and.arrow.down")
-                        .foregroundColor(.black)
-                }.alert(isPresented: $showSaveAlert) {
-                    Alert(
-                        title: Text("\(projectName) Saved"),
-                        dismissButton: .default(Text("OK"))
-                    )
+                )
+            }
+            ToolbarItemGroup(placement: .topBarTrailing) {
+                Button("Save") {
+                    dataCanvasViewModel.saveProject(viewContext: viewContext)
+                  
                 }
             }
         }
-        .fullScreenCover(isPresented: $isRoomCaptureViewPresented) {
-            ZStack {
-                OnboardingViewControllerWrapper()
-                VStack {
-                    HStack {
-                        Button("Cancel") {
-                            isRoomCaptureViewPresented = false
-                        }.buttonStyle(.automatic)
-                        
-                        Spacer()
-                    }
-                    .padding()
-                    
-                    Spacer()
-                }
-            }
-        }
-        .navigationTitle(projectName)
-        .toolbarTitleMenu {
-            Button(action: {
-                renameClicked = true
-                print("Action for context menu item 1")
-            }) {
-                Label("Rename", systemImage: "pencil")
-            }
-            Button(action: {
-                isExporting = true
-                print("Action for context menu item 2")
-            }) {
-                Label("Export as USDZ", systemImage: "square.and.arrow.up")
-            }
-        }
-        .alert("Rename", isPresented: $renameClicked, actions: {
-            TextField("\(projectName)", text: $newProjectName)
-            Button("Save", action: {
-                projectName = newProjectName
-                renameClicked = false
-            })
-            Button("Cancel", role: .cancel, action: {
-                renameClicked = false
-            })
-        })
-        
+        .toolbarBackground(Color(UIColor.systemGray6), for: .navigationBar)
+        .toolbarBackground(.visible, for: .navigationBar)
+        .navigationBarBackButtonHidden(true)
     }
 }
 
-
-
-#Preview {
-    CanvasView(objectsButtonClicked: false, roomButtonClicked: false, viewfinderButtonClicked: .constant(false), isImporting: .constant(false), isExporting: .constant(false), isSetButtonSidebarTapped: .constant(false))
+#if DEBUG
+struct CanvasView_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationStack{
+            CanvasView().environment(\.managedObjectContext,PersistenceController.preview.container.viewContext)
+                .environmentObject(RouterView())
+        }
+    }
 }
+#endif
