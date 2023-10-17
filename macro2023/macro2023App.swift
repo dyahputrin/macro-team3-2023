b4be1d749f48d20dd106cea137d94cfe44e50960
biljef232 <74884918+biljef232@users.noreply.github.com>
2023-10-17 13:59:02 +0700

    udah jadi
    heh

--- macro2023/macro2023App.swift
+++ macro2023/macro2023App.swift
@@ -10,10 +10,12 @@ import SwiftUI
 @main
 struct macro2023App: App {
     let persistenceController = PersistenceController.shared
+    @StateObject var routerView = RouterView()
     var body: some Scene {
         WindowGroup {
             ContentView()
                 .environment(\.managedObjectContext,persistenceController.container.viewContext)
+                .environmentObject(routerView)
         }
     }
 }
