54084ccdacd2b28e6aa6145728d5c7b28c716f3a
Oey Darryl Valencio Wijaya <70695383+iDreal88@users.noreply.github.com>
2023-10-18 16:45:53 +0700

    update sidebar

---
+++ macro2023/Sidebar.swift
@@ -0,0 +1,18 @@
+//
+//  Sidebar.swift
+//  macro2023
+//
+//  Created by Oey Darryl Valencio Wijaya on 18/10/23.
+//
+
+import SwiftUI
+
+struct Sidebar: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Sidebar()
+}
