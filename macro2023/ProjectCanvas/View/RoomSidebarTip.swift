4f3c216cc49c215214784ad1ddfc4eec8c69077b
Oey Darryl Valencio Wijaya <70695383+iDreal88@users.noreply.github.com>
2023-11-15 20:21:15 +0700

    TipKit

---
+++ macro2023/ProjectCanvas/View/RoomSidebarTip.swift
@@ -0,0 +1,18 @@
+//
+//  RoomSidebarTip.swift
+//  macro2023
+//
+//  Created by Oey Darryl Valencio Wijaya on 15/11/23.
+//
+
+import SwiftUI
+
+struct RoomSidebarTip: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RoomSidebarTip()
+}
