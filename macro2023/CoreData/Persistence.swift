87d3a9f82d4d49b3dab59caaf4c946bd7aef2fc7
Jefry Gunawan <75528740+Jefry-Gunawan@users.noreply.github.com>
2023-11-22 13:51:00 +0700

    update
    abis merge

--- macro2023/CoreData/Persistence.swift
+++ macro2023/CoreData/Persistence.swift
@@ -23,7 +23,7 @@ struct PersistenceController {
         }
         container.loadPersistentStores(completionHandler: { (storeDescription, error) in
             if let error = error as NSError? {
-                fatalError("Unresolved error \(error), \(error.userInfo)")
+//                fatalError("Unresolved error \(error), \(error.userInfo)")
             }
         })
         container.viewContext.automaticallyMergesChangesFromParent = true
